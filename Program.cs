d5fb4c55e963a2e3424fdb5f8a46bd9e6dba420f
workaholic <p1h2c3@hotmail.com>
2022-05-25 01:51:27 +0000

    First dotnet project

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+TestOutput();
+
+static void TestOutput() {
+    Console.WriteLine("WTF?");
+}
